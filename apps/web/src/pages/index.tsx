8df60d3b9e5f771d97040b95e26e51d5a9440ef4
nyxb <contact@nyxb.xyz>
2023-05-28 09:54:36 +0200

    🎨 style(index.tsx): fix typo in className property value The className property value was incorrectly set to 'text=white' instead of 'text-white' in the ReactMarkdown component. This caused the text to not be displayed in white color as intended.

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -166,12 +166,12 @@ export default function Web(props: { error: string; errors: ErrorInfo[]; lang: s
                     <>
                       <h2 className='text-nice-yellow'>{t('translation')}</h2>
                       <div className='p-4 py-3 font-light text-gray-100 bg-gray-800 rounded prose-code:text-white prose-p:m-0'>
-                        <ReactMarkdown>
+                        <ReactMarkdown className='text=white'>
                           {error.improvedError.excerpt}
                         </ReactMarkdown>
                       </div>
                        <h2 className='text-nice-yellow'>{t('explanation')}</h2>
-                      <ReactMarkdown>{error.improvedError?.body}</ReactMarkdown>
+                      <ReactMarkdown className='text=white'>{error.improvedError?.body}</ReactMarkdown>
                     </>
                   )}
                   {!error.improvedError && (
