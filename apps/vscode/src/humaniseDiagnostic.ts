bd7a234d6d7da748e47267e7e7438495905500c4
nyxb <contact@nyxb.xyz>
2023-05-28 07:45:54 +0200

    🌐 feat(humaniseDiagnostic.ts): add support for multiple languages The `humaniseDiagnostic` function now accepts a `language` parameter, which is used to determine which language file to use for error translations. The `bundleErrors` import has been replaced with two separate imports for English and German translations. The `getMessageTemplate` function has been refactored to remove unnecessary if-else statements. The link to the translation website has been updated to point to the new website.

--- apps/vscode/src/humaniseDiagnostic.ts
+++ apps/vscode/src/humaniseDiagnostic.ts
@@ -1,40 +1,36 @@
 import * as vscode from 'vscode'
-
 import {
    fillBodyAndExcerptWithItems,
    parseErrors,
 } from '@nyxb/error-translation-engine'
 import { compressToEncodedURIComponent } from 'lz-string'
-import * as bundleErrors from './bundleErrors.json'
+
 import type { Options } from './types'
+import * as bundleErrorsEn from './bundleErrors_en.json'
+import * as bundleErrorsDe from './bundleErrors_de.json'
 
 function getMessageTemplate(options: Options) {
-   let messageTemplate:
-   | 'body-and-tldr'
-   | 'tldr-only'
-   | 'body-only'
-   | 'link-only'
+   let messageTemplate: 'body-and-tldr' | 'tldr-only' | 'body-only' | 'link-only'
 
    if (options.showFullTranslation) {
       if (options.showTLDRTranslation)
          messageTemplate = 'body-and-tldr'
-
-      else
-         messageTemplate = 'body-only'
+      else messageTemplate = 'body-only'
    }
    else {
       if (options.showTLDRTranslation)
          messageTemplate = 'tldr-only'
-
-      else
-         messageTemplate = 'link-only'
+      else messageTemplate = 'link-only'
    }
 
    return messageTemplate
 }
 
-export function humaniseDiagnostic(diagnostic: vscode.Diagnostic,
-   options: Options): vscode.MarkdownString[] {
+export function humaniseDiagnostic(
+   diagnostic: vscode.Diagnostic,
+   options: Options,
+   language: string,
+): vscode.MarkdownString[] {
    if (diagnostic.source !== 'ts')
       return []
 
@@ -42,12 +38,24 @@ export function humaniseDiagnostic(diagnostic: vscode.Diagnostic,
 
    const markdownStrings: vscode.MarkdownString[] = []
 
-   errors.forEach((error, index, array) => {
+   errors.forEach((error) => {
       const errorBodies: string[] = []
 
-      const fullError = (
-         bundleErrors as Record<string, { body: string; excerpt: string }>
-      )[error.code]
+      let bundleErrors
+      if (language === 'en') {
+         bundleErrors = bundleErrorsEn as Record<
+            string,
+            { body: string; excerpt: string }
+         >
+      }
+      else {
+         bundleErrors = bundleErrorsDe as Record<
+            string,
+            { body: string; excerpt: string }
+         >
+      }
+
+      const fullError = bundleErrors[error.code]
 
       errorBodies.push(['```txt', error.parseInfo.rawError, '```'].join('\n'))
 
@@ -60,20 +68,15 @@ export function humaniseDiagnostic(diagnostic: vscode.Diagnostic,
 
          const messageTemplate = getMessageTemplate(options)
 
-         const linkToTranslation = `[See full translation](https://ts-error-translator.vercel.app/?error=${compressToEncodedURIComponent(
-        diagnostic.message,
-      )})`
+         const linkToTranslation = `[See full translation](https://typescript-wizard.vercel.app/?error=${compressToEncodedURIComponent(
+            diagnostic.message,
+         )})`
 
          switch (messageTemplate) {
-                  case 'body-and-tldr':
-                     {
-                        errorBodies.push(
-              `**Translation**: ${excerpt}`,
-              body,
-              linkToTranslation,
-                        )
-                     }
+                  case 'body-and-tldr': {
+                     errorBodies.push(`**Translation**: ${excerpt}`, body, linkToTranslation)
                      break
+                  }
                   case 'body-only':
                      errorBodies.push(linkToTranslation, body)
                      break
@@ -87,7 +90,7 @@ export function humaniseDiagnostic(diagnostic: vscode.Diagnostic,
       }
       else {
          errorBodies.push(
-        `[Contribute a translation for \`#${error.code}\`](https://github.com/mattpocock/ts-error-translator/blob/main/CONTRIBUTING.md)`,
+            `[Contribute a translation for \`#${error.code}\`](https://github.com/mattpocock/typescript-wizard/blob/main/CONTRIBUTING.md)`,
          )
       }
 
