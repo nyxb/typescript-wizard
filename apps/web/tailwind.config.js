13d8cbb98f0e79cb93957dc1152cced9136edf76
nyxb <contact@nyxb.xyz>
2023-05-28 07:52:59 +0200

    🎨 style(tailwind.config.js): add custom colors to the theme The tailwind.config.js file has been updated to include custom colors in the theme. The colors 'nice-green', 'nice-purple', and 'nice-yellow' have been added to the theme's color palette. This change improves the visual design of the application by providing more color options to use in the UI.

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')],
-};
+   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+   theme: {
+      extend: {
+         colors: {
+            'nice-green': '#14F195',
+            'nice-purple': '#9945FF',
+            'nice-yellow': 'hsl(53, 100%, 50%)',
+         },
+      },
+   },
+   plugins: [require('@tailwindcss/typography')],
+}
