444ea5092b4915b81b47ac02bf0380231548712e
nyxb <contact@nyxb.xyz>
2023-05-28 08:02:52 +0200

    🚀 feat(translate.ts): add support for multiple languages 🐛 fix(translate.ts): fix typo in English excerpt The script now accepts a second argument for the language of the translation. The getTemplate function now takes the language argument and returns a template for the specified language. The English excerpt had a typo and has been fixed.

--- scripts/translate.ts
+++ scripts/translate.ts
@@ -3,38 +3,53 @@ import tsErrorMessages from '../packages/engine/src/tsErrorMessages.json'
 import { logger } from './utils/logger'
 
 const errorCode = process.argv[2]
+const language = process.argv[3]
 
-function getTemplate(original: string) {
-   return `---
+function getTemplate(original: string, language: string) {
+   let template = ''
+   if (language === 'en') {
+      template = `---
 original: "${original}"
-excerpt: "Simplified version of the error message"
+excerpt: "Simplified version of the error message in English"
 ---
 
-More details, reproducible examples for the error and how the error should be fixed.
+More details, reproducible examples for the error and how the error should be fixed in English.
 
 `.trim()
+   }
+   else if (language === 'de') {
+      template = `---
+original: "${original}"
+excerpt: "Vereinfachte Version der Fehlermeldung auf Deutsch"
+---
+
+Weitere Details, reproduzierbare Beispiele für den Fehler und wie der Fehler behoben werden sollte auf Deutsch.
+
+`.trim()
+   }
+   return template
 }
 
-if (!errorCode) {
-   logger.error('\nYou haven\'t provided an error code for your translation!')
+if (!errorCode || !language) {
+   logger.error('\nYou haven\'t provided an error code or language for your translation!')
    logger.info('Example:')
-   logger.info('  yarn translate 1006')
+   logger.info('  pnpm translate 1006 en or pnpm translate 1006 de')
    process.exit(1)
 }
 else {
-   const errorPath = `packages/engine/errors/${errorCode}.md`
+   const errorPath = `packages/engine/errors/${language}/${errorCode}.md`
 
    if (fs.existsSync(errorPath)) {
-      logger.error(`\nTranslation for error code ${errorCode} already exists!`)
+      logger.error(`\nTranslation for error code ${errorCode} already exists in ${language}!`)
    }
    else {
       const originalError = Object.keys(tsErrorMessages).find((key) => {
          return String(tsErrorMessages[key].code) === errorCode
       })
       // @ts-expect-error is fine
-      fs.writeFileSync(errorPath, getTemplate(originalError))
+      fs.writeFileSync(errorPath, getTemplate(originalError, language))
 
-      logger.success('\nTemplate has been written Successfuly!')
+      logger.success('\nTemplate has been written successfully!')
       logger.info(`Check: ${errorPath}`)
    }
 }
